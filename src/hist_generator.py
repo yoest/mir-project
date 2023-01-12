4eceb0e9cebfba5329d56d0f5299d5a277250b68
Hugo Venturoso <hugo@MacBook-Air-de-Hugo-3.local>
2023-01-12 16:30:46 +0100

    End of the indexation

--- src/hist_generator.py
+++ src/hist_generator.py
@@ -1,7 +1,9 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
+#from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
 import os
 import cv2
+from utils import *
 import numpy as np
+
 from skimage.transform import resize
 from skimage.feature import hog
 from skimage import exposure
@@ -9,7 +11,57 @@ from skimage import io, color, img_as_ubyte
 from matplotlib import pyplot as plt
 from skimage.feature import hog, greycomatrix, graycoprops, local_binary_pattern, greycoprops
 
+def check_or_create(output_dir):
+    #output_dir = "../output/" + output_dir
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
 
+def global_generator(filenames, progress_bar, feature_methods):
+    #output_dir = "../output/BGR_HSV_GLCM_LPB_HOG_SIFT_ORB"
+    """given a list of features methods (BGR, HSV, GLCM, ...), combine only the descriptors present in features_methods"""
+    dir = compute_feature_methods_name(feature_methods)
+    output_dir = "../output/" + dir
+    
+    check_or_create(output_dir)
+
+    for i, path in enumerate(os.listdir(filenames)):
+        img = cv2.imread(filenames + "/" + path)
+        
+        dec_sift, dec_orb = None, None
+        
+        features = []
+       
+        if "BGR" in feature_methods:
+            color_hist = compute_colors(img)
+            features.append(color_hist)
+        if "HSV" in feature_methods:
+            hsv_hist = compute_hsv(img)
+            features.append(hsv_hist)
+        if "GLCM" in feature_methods:
+            glcm = compute_glcm(img)
+            features.append(glcm)
+        if "LBP" in feature_methods:
+            lbp = compute_lbp(img)
+            features.append(lbp)
+        if "HOG" in feature_methods:
+            hog = compute_hog(img)
+            features.append(hog)
+        if "SIFT" in feature_methods:
+            kps_sift, dec_sift = compute_sift(img)
+            if dec_sift is not None:
+                features.append(dec_sift)
+        if "ORB" in feature_methods:
+            kps_orb, dec_orb = compute_orb(img)
+            if dec_orb is not None:
+                features.append(dec_orb)
+       
+        feature = np.concatenate(features, axis=None)
+        num_image, _ = path.split(".")
+        np.savetxt(output_dir + "/" + str(num_image) + ".txt", feature)
+        progress_bar.setValue(100 * ((i + 1) / len(os.listdir(filenames))))
+
+    progress_bar.setValue(0)
+    print(f"Indexing {dir} done")
 
 def generate_color_hist(filenames, progress_bar):
     """ Generate color histogram for each image in the folder """
@@ -18,11 +70,8 @@ def generate_color_hist(filenames, progress_bar):
 
     for i, path in enumerate(os.listdir(filenames)):
         img = cv2.imread(filenames + "/" + path)
-        hist_b = cv2.calcHist([img], [0], None, [256], [0,256])
-        hist_g = cv2.calcHist([img], [1], None, [256], [0,256])
-        hist_r = cv2.calcHist([img], [2], None, [256], [0,256])
-        feature = np.concatenate((hist_b, np.concatenate((hist_g, hist_r), axis=None)), axis=None)
-
+        feature = compute_colors(img)
+        
         num_image, _ = path.split(".")
         np.savetxt("../output/BGR/" + str(num_image) + ".txt", feature)
         progress_bar.setValue(100 * ((i + 1) / len(os.listdir(filenames))))
@@ -38,21 +87,13 @@ def generate_hsv_hist(filenames, progress_bar):
 
     for i, path in enumerate(os.listdir(filenames)):
         img = cv2.imread(filenames + "/" + path)
-        hsv_image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        hist_h = cv2.calcHist([hsv_image], [0], None, [256], [0,256])
-        hist_s = cv2.calcHist([hsv_image], [1], None, [256], [0,256])
-        hist_v = cv2.calcHist([hsv_image], [2], None, [256], [0,256])
-
-        feature = np.concatenate((hist_v, np.concatenate((hist_s, hist_h), axis=None)), axis=None)
-
+        feature = compute_hsv(img)
         num_image, _ = path.split(".")
         np.savetxt("../output/HSV/" + str(num_image) + ".txt", feature)
         progress_bar.setValue(100 * ((i + 1) / len(os.listdir(filenames))))
     
     progress_bar.setValue(0)
     print("Indexing hist hsv done")
-
-
 
 def reduce_resolution(img, new_width, new_height):
 
@@ -67,23 +108,15 @@ def generate_sift(filenames, progress_bar):
         os.mkdir("../output/SIFT")
 
     for i, path in enumerate(os.listdir(filenames)):
-        old_img = cv2.imread(filenames + "/" + path)
-        img = reduce_resolution(old_img, 400, 300)
+        img = cv2.imread(filenames + "/" + path)
+        kps, des = compute_sift(img)
         featureSum = 0
-        sift = cv2.xfeatures2d.SIFT_create()
-        #print(img.shape)
-        kps , des = sift.detectAndCompute(img,None)
-
         num_image, _ = path.split(".")
-        #print(len(kps), "len(kps)") 
-        #print(len(des), "len(des)") 
+       
         if(len(kps)) > 0:
             np.savetxt("../output/SIFT/" + str(num_image) + ".txt" ,des)
             progress_bar.setValue(100 * ((i + 1) / len(os.listdir(filenames))))
-        
             featureSum += len(kps)
-        else:
-            print(filenames + "/" + path)
     
     progress_bar.setValue(0)
     print("Indexing sift done")
@@ -97,15 +130,14 @@ def generate_orb(filenames, progress_bar):
 
     for i, path in enumerate(os.listdir(filenames)):
         img = cv2.imread(filenames + "/" + path)
-        orb = cv2.ORB_create()
-        key_point1, descriptor1 = orb.detectAndCompute(img, None)
+        
+        key_point1, descriptor1 = compute_orb(img)
         
         num_image, _ = path.split(".")
         if(descriptor1 is not None):
             np.savetxt("../output/ORB/" + str(num_image) + ".txt", descriptor1)
             progress_bar.setValue(100 * ((i + 1) / len(os.listdir(filenames))))
-        else:
-            print(filenames + "/" + path)
+        
 
     progress_bar.setValue(0)
     print("Indexing orb done")
@@ -116,21 +148,12 @@ def generate_glcm(filenames, progress_bar):
     if not os.path.isdir("../output/GLCM"):
         os.mkdir("../output/GLCM")
 
-    distances = [1,-1]
-    angles = [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4]
+
 
     for i, path in enumerate(os.listdir(filenames)):
-        image = cv2.imread(filenames + "/" + path)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        gray = img_as_ubyte(gray)
-        glcm_matrix = greycomatrix(gray, distances=distances, angles=angles, normed=True)
-        glcm_prop_1 = graycoprops(glcm_matrix, 'contrast').ravel()
-        glcm_prop_2 = graycoprops(glcm_matrix, 'dissimilarity').ravel()
-        glcm_prop_3 = graycoprops(glcm_matrix, 'homogeneity').ravel()
-        glcm_prop_4 = graycoprops(glcm_matrix, 'energy').ravel()
-        glcm_prop_5 = graycoprops(glcm_matrix, 'correlation').ravel()
-        glcm_prop_6 = graycoprops(glcm_matrix, 'ASM').ravel()
-        feature = np.array([glcm_prop_1, glcm_prop_2, glcm_prop_3, glcm_prop_4, glcm_prop_5, glcm_prop_6]).ravel()
+        img = cv2.imread(filenames + "/" + path)
+
+        feature = compute_glcm(img)
         num_image, _ = path.split(".")
         np.savetxt("../output/GLCM/" + str(num_image) + ".txt", feature)
         progress_bar.setValue(100 * ((i + 1) / len(os.listdir(filenames))))
@@ -146,19 +169,9 @@ def generate_lbp(filenames, progress_bar):
 
     for i, path in enumerate(os.listdir(filenames)):
         img = cv2.imread(filenames + "/" + path)
-        points = 8
-        radius = 1
-        method='default'
-        sub_size = (70, 70)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = cv2.resize(img, (350, 350))
-        full_lbp_matrix = local_binary_pattern(img, points, radius, method)
-        histograms = []
-        for k in range(int(full_lbp_matrix.shape[0] / sub_size[0])):
-            for j in range(int(full_lbp_matrix.shape[1] / sub_size[1])):
-                sub_vector = full_lbp_matrix[k * sub_size[0]:(k+1) * sub_size[0], j * sub_size[1]:(j+1)*sub_size[1]].ravel()
-                sub_hist, _ = np.histogram(sub_vector, bins=int(2**points), range=(0, 2**points))
-                histograms = np.concatenate((histograms, sub_hist), axis=None)
+        
+        histograms = compute_lbp(img)
+        
         num_image, _ = path.split(".")
         np.savetxt("../output/LBP/" + str(num_image) + ".txt", histograms)
         progress_bar.setValue(100 * ((i + 1) / len(os.listdir(filenames))))
@@ -172,61 +185,13 @@ def generate_hog(filenames, progress_bar):
     if not os.path.isdir("../output/HOG"):
         os.mkdir("../output/HOG")
 
-    cell_size = (25, 25)
-    block_size = (50, 50)
-    block_stride = (25, 25)
-    n_bins = 9
-    win_size = (350, 350)
     for i, path in enumerate(os.listdir(filenames)):
         img = cv2.imread(filenames + "/" + path)
-        image = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        image = cv2.resize(image, win_size)
-        hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, n_bins)
-        feature = hog.compute(image)
+        
+        feature = compute_hog(img)
         num_image, _ = path.split(".")
         np.savetxt("../output/HOG/" + str(num_image) + ".txt", feature)
         progress_bar.setValue(100 * ((i + 1) / len(os.listdir(filenames))))
 
     progress_bar.setValue(0)
     print("Indexing hog done")
-
-""" J'ai essayé un truc mais pas sur que ce soit la bonne approche, je comprends pas trop comment on peut combiner
-def global_generator(filenames, progress_bar, feature_methods):
-    if not os.path.isdir("FEATURES"):
-        os.mkdir("FEATURES")
-        
-    # Create a dictionary mapping descriptor names to feature extraction functions
-    descriptor_functions = {
-        "SIFT": cv2.SIFT_create,
-        "ORB": cv2.ORB_create,
-        # Ajouter les autres
-    }
-    
-    for i, path in enumerate(os.listdir(filenames)):
-        img = cv2.imread(filenames + "/" + path)
-        num_image, _ = path.split(".")
-        
-        # Initialize an empty list to store all the features for this image
-        features = []
-        
-        for descriptor in feature_methods:
-            # Extract features using the appropriate function from the dictionary
-            descriptor_function = descriptor_functions[descriptor]
-            feature_extractor = descriptor_function()
-            _, descriptor1 = feature_extractor.detectAndCompute(img, None)
-            
-            # Make sure all feature arrays have the same number of dimensions
-            if len(descriptor1.shape) == 1:
-                descriptor1 = descriptor1.reshape(-1, 1)
-            
-            # Add the features to the list
-            features.extend(descriptor1)
-        
-        # Convert the list of features to a numpy array and save it to a file
-        features = np.array(features)
-        np.savetxt("FEATURES/" + str(num_image) + ".txt", features)
-        
-        progress_bar.setValue(100 * ((i + 1) / len(os.listdir(filenames))))
-    
-    print("Indexing features done")
-"""
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,11 @@
 import os
 import time
+import cv2
+import numpy as np
+from skimage.transform import resize
+import cv2
+import numpy as np
+from skimage.feature import hog, greycomatrix, graycoprops, local_binary_pattern, greycoprops
 
 
 def compute_execution_time(fct, args):
@@ -9,6 +15,11 @@ def compute_execution_time(fct, args):
     end = time.time()
     print(f'Time taken (in seconds): {end - start}')
 
+def compute_feature_methods_name(feature_methods):
+    res = ""
+    for method in feature_methods:
+        res = res + "_" + method 
+    return res 
 
 def compute_file_size(filename):
     """ Compute the size of a file given by its name. """
@@ -16,3 +27,85 @@ def compute_file_size(filename):
 
     print(f'File Size in Bytes is {file_stats.st_size}')
     print(f'File Size in MegaBytes is {file_stats.st_size / (1024 * 1024)}')
+
+
+def compute_colors(img):
+    """ Compute color vectors features for an image """
+    hist_b = cv2.calcHist([img], [0], None, [256], [0,256])
+    hist_g = cv2.calcHist([img], [1], None, [256], [0,256])
+    hist_r = cv2.calcHist([img], [2], None, [256], [0,256])
+    return np.concatenate((hist_b, np.concatenate((hist_g, hist_r), axis=None)), axis=None)
+
+def compute_hsv(img):
+    """ Compute hsv vectors features for an image """
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hist_h = cv2.calcHist([hsv], [0], None, [256], [0,256])
+    hist_s = cv2.calcHist([hsv], [1], None, [256], [0,256])
+    hist_v = cv2.calcHist([hsv], [2], None, [256], [0,256])
+    return np.concatenate((hist_h, np.concatenate((hist_s, hist_v), axis=None)), axis=None)
+
+def compute_glcm(img):
+    """ Compute GLCM features for an image """
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    image_glcm = greycomatrix(img, distances=[1], angles=[0, np.pi / 4, -np.pi / 2], symmetric=True, normed=True)
+    return greycoprops(image_glcm, 'contrast')
+
+
+def compute_lbp(image):
+    """ Compute LBP features for an image """
+    def cal_hist_gray(image):
+        image = image.astype(np.uint8)
+        hist = cv2.calcHist(image, [0], None, [256], [0, 256])
+        hist = cv2.normalize(hist, hist)
+        return hist
+
+    def lbp_descriptor(image):             
+        METHOD = 'uniform'
+        radius = 3
+        n_points = 8 * radius
+        gray = cal_hist_gray(image)
+        lbp = local_binary_pattern(gray, n_points, radius, METHOD)
+        return lbp
+
+    def cal_lbp(image):                   
+        if image is not None:
+            des = lbp_descriptor(image)
+            lbp_features = des.tolist()
+        lbp_features = np.array(lbp_features)
+        return lbp_features
+
+    #image = cv2.imread(filename, 3)
+    return cal_lbp(image)
+
+def compute_hog(image):
+    """ Compute HOG features for an image """
+    def hog_desc(image):
+        resized_img = resize(image, (128*4, 64*4))
+        fd, hog_image = hog(resized_img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=True, multichannel=True)
+        return hog_image
+
+    #image = cv2.imread(filename, 3)
+    return hog_desc(image)
+
+def reduce_resolution(img, new_width, new_height):
+
+    # Réduire la résolution de l'image
+    red_img = cv2.resize(img, (new_width, new_height))
+    return red_img
+
+def compute_sift(old_img):
+        #old_img = cv2.imread(filename)
+        img = reduce_resolution(old_img, 200, 150)
+        sift = cv2.xfeatures2d.SIFT_create()
+        kps, des = sift.detectAndCompute(img,None)
+        
+        return kps, des
+            
+def compute_orb(img):
+    orb = cv2.ORB_create()
+    key_point1, descriptor1 = orb.detectAndCompute(img, None)
+    return key_point1, descriptor1    
+    
+
+
+       
